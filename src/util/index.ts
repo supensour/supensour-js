92c8c373bd462bd37df74fa4824554fae8bd0b64
SuprayanY <SuprayanY@yahoo.com>
2022-01-31 02:32:45 +0700

    mono repo parent

--- src/util/index.ts
+++
@@ -1,6 +0,0 @@
-import { Optional } from '@/util/optional'
-
-export * as error from '@/util/error'
-export * as func from '@/util/func'
-
-export { Optional }
--- test/unit/specs/index.spec.ts
+++
@@ -1,9 +0,0 @@
-import * as util from '@/index'
-
-describe('index.ts', () => {
-  it('export', () => {
-    const keys = Object.keys(util)
-    expect(keys.length).toEqual(1)
-    expect(keys.includes('util')).toBe(true)
-  })
-})
